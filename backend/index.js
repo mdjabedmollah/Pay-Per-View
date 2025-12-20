ef72836a0bb0ae5d016b5f812888dadf3659eeb8
mdjabedmollah <mdjabed2167@gmail.com>
2025-12-20 20:00:20 +0600

    middleware

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 8081
 
+//middleware 
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true,              
+  })
+);
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
